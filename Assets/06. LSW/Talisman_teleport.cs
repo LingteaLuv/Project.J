7bc1988b9acaf778a602452da0306c55a20c274d
01stone01 <hagwhr2@gmail.com>
2025-04-29 00:19:39 +0900

    [Feat] Add Talisman
    폭발 부적, 이동 부적 추가

--- Assets/06. LSW/Talisman_teleport.cs
+++ Assets/06. LSW/Talisman_teleport.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TalismanTeleport : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float liveTime;
+    [SerializeField] private LayerMask ignoreLayer;
+    private float _timer;
+    
+    private void OnEnable()
     {
-        
+        _timer = 0;
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void Update()
     {
+        _timer += Time.deltaTime;
+        if (_timer > liveTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer == ignoreLayer) return;
         
+        Destroy(gameObject);
     }
 }
